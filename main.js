402773fd02372cd155dc8a48e1a5548c8753403a
Haneen <htsalhagbani@gmail.com>
2024-09-08 12:07:11 +0300

    JS code1

---
+++ main.js
@@ -0,0 +1,38 @@
+let height=4;
+let width=7;
+console.log(`The area of the rectangle is:${height*width}`);
+
+
+let degree=40;
+let F=(degree*9/5)+32;
+let C=(degree-32)*9/5;
+console.log(`NN°F ${F} NN°C ${C}`);
+
+let hour=1;
+let seconds = hour * 60 * 60;
+console.log(`${hour} hours =  ${seconds} Seconds`);
+
+let Name="Haneen";
+let family="Alhagbani";
+console.log(Name + " " + family);
+
+
+var bill=10.25 + 3.99 + 7.15;
+var tip= bill*0.15;
+var total= bill+tip;
+console.log(`total: ${total}`);
+
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`;
+console.log(madLib);
+
+
+var firstName="Noura";
+var interest="Learn Js";
+var hobby="Watch TV";
+var awesomeMessage="Hi, my name is _____. I love ______. In my spare time, I like to ______.";
+
+console.log(`Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`);
